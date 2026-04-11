f5d86e11fda882795f3ee83c594a2fda828d62c5
saurabh <saurabh20tiwari04@gmail.com>
2026-04-11 18:28:45 +0530

    implemented sign in and sign out functionality

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,19 +1,14 @@
-import { signup } from '#controllers/auth.controller.js';
+import { signin, signout, signup } from '#controllers/auth.controller.js';
 import express from 'express'
 
 const router = express.Router();
 
 router.post('/sign-up', signup);
 
-
-router.post('/sign-in', (req, res) => {
- res.send('POST /api/auth/sign-in rseponse');
-});
+router.post('/sign-in', signin);
 
 
-router.post('/sign-out', (req, res) => {
- res.send('POST /api/auth/sign-out rseponse');
-});
+router.post('/sign-out', signout);
 
 
 export default router;
